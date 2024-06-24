122985987b427b2850150b2d94e9aeb94417f48e
pavlenko <PavlenkoED@kamaz.ru>
2024-06-24 11:01:56 +0300

    Second step + gitignore added

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,11 +6,13 @@ def main():
 
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
+    parser.add_argument('-f', '--format', type=str, help='Sets format of output')
 
     args = parser.parse_args()
 
     print(args.first_file)
     print(args.second_file)
+    print(args.format)
 
 
 if __name__ == '__main__':
